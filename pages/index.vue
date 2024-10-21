83f529e28262a3c4e28f63ec187ad8998945ec48
Fedorov Denis <ssboshen@yandex.ru>
2024-10-21 11:11:40 +0300

    add select menu

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+	<section class="flex items-center justify-between mb-10">
+		<h1 class="text-4xl font-extrabold">Summary</h1>
+		<div>
+			<USelectMenu
+				:v-model="selectedView"
+				:options="transactionView"
+			/>
+		</div>
+	</section>
+</template>
+
+<script setup>
+const transactionView = ['Yearly', 'Monthly', 'Weekly'];
+const selectedView = ref(transactionView[1]);
+</script>
+
+<style lang="scss" scoped></style>
