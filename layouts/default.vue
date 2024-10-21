d8db66cecb94e2233bd607ff7b8e644b912cb1f0
Fedorov Denis <ssboshen@yandex.ru>
2024-10-21 12:21:33 +0300

    add select menu logic

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container mx-auto max-w-4xl">
 		<AppHeader />
-    <main class="my-10">
+    <main class="mt-10">
       <slot />
     </main>
   </div>
--- pages/index.vue
+++ pages/index.vue
@@ -3,16 +3,16 @@
 		<h1 class="text-4xl font-extrabold">Summary</h1>
 		<div>
 			<USelectMenu
-				:v-model="selectedView"
-				:options="transactionView"
+				v-model="selectedView"
+				:options="transactionViewOptions"
 			/>
 		</div>
 	</section>
 </template>
 
 <script setup>
-const transactionView = ['Yearly', 'Monthly', 'Weekly'];
-const selectedView = ref(transactionView[1]);
+import { transactionViewOptions } from '~/constants';
+const selectedView = ref(transactionViewOptions[1]);
 </script>
 
 <style lang="scss" scoped></style>
